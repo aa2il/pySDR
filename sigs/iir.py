0fd3cd267ed79ae568336640da65c5a53c3704ad
aa2il <jattili@yahoo.com>
2024-04-09 11:09:18 -0700

    Updates

--- sigs/iir.py
+++ sigs/iir.py
@@ -35,20 +35,22 @@ def plot_freq_response(b,a,fs,btype):
     plt.grid()
     plt.show()
     
+##############################################################################
     
 # Example 1 - BPF or BAND STOP 50-200 Hz
-if False:
+if 0:
     fs=1000
-    btype='band'
-    btype='stop'
+    btype='band'             # Bandpass
+    #btype='stop'            # Bandstop
     b,a = signal.iirfilter(15, [50, 200], rp=3,rs=100,
                            btype=btype, analog=False, ftype='cheby2',fs=fs)
     plot_freq_response(b,a,fs,btype)
+    sys.exit(0)
 
 ##############################################################################
 
 # Example 2 - Notch 50 Hz filter
-if False:
+if 1:
     fs               = 1000      # Sampling freq
     notch_freq       = 50.0      # Frequency to be removed from signal (Hz)
     quality_factor   = 20.0      # Quality factor
@@ -73,27 +75,40 @@ if False:
     plt.title('Noisy Signal', fontsize=20)
     
     # Apply notch filter to the noisy signal
-    #y = signal.filtfilt(b, a, x)    # Fwd & bkwd - suitable for off-line processing
-    #zi = signal.lfilter_zi(b, a)          # Not worth the trouble
+    #y = signal.filtfilt(b, a, x)     # Fwd & bkwd - suitable for off-line processing
+    #zi = signal.lfilter_zi(b, a)     # Not worth the trouble
     #print('zi=',zi)
     y = signal.lfilter(b, a, x)      # Fwd direction only - more realistic for real-time
     
     # Now let's see if we can figure out how to do processing in chunks - LOOKING GOOD!
+    # Break input sig into two chunks
     N=len(x)
     N2=int(N/2)
     print('N=',N,N2)
     x1=x[0:N2]
     x2=x[N2:]
-    zi = (max(len(a), len(b)) - 1)*[0]
+    zi = (max(len(a), len(b)) - 1)*[0]                  # Need this so we can get z1, the state at the end
     y1,z1 = signal.lfilter(b, a, x1,zi=zi) 
     y2,z2 = signal.lfilter(b, a, x2,zi=z1 )
     yy = np.concatenate( (y1, y2) )
-    print(np.size(t),np.size(y),np.size(y1),np.size(y2),np.size(yy))
+    #print(np.size(t),np.size(y),np.size(y1),np.size(y2),np.size(yy))
+
+    # Break input sig into three chunks - LOOKING GOOD!
+    N3=int(N/3)
+    x1=x[0:N3]
+    x2=x[N3:2*N3]
+    x3=x[2*N3:]
+    zi = (max(len(a), len(b)) - 1)*[0]
+    y1,z1 = signal.lfilter(b, a, x1,zi=zi) 
+    y2,z2 = signal.lfilter(b, a, x2,zi=z1 )
+    y3,z3 = signal.lfilter(b, a, x3,zi=z2 )
+    yyy = np.concatenate( (y1, y2, y3) )
     
     # Plot filtered signal
     plt.subplot(212)
     plt.plot(t, y)
     plt.plot(t, yy,'r:')
+    plt.plot(t, yyy,'g:')
     plt.xlabel('Time (sec)', fontsize=20)
     plt.ylabel('Magnitude (dB)', fontsize=18)
     plt.title('Filtered Signal', fontsize=20)
@@ -101,10 +116,20 @@ if False:
     fig.tight_layout()
     plt.show()
 
+    dy=y-yy
+    print('dy=',dy)
+    print(max(abs(dy)))
+    
+    dy=y-yyy
+    print('dy=',dy)
+    print(max(abs(dy)))
+    
+    sys.exit(0)
+
 ##############################################################################
 
 # Example 3 - LPF 200 Hz
-if True:
+if 0:
     fs=8000
     b, a = signal.iirfilter(7, 200, rp=3,rs=100,
                             btype='low', analog=False, ftype='ellip',fs=fs)
@@ -112,7 +137,7 @@ if True:
 
     # Create test signal that is a mixture of several different frequencies + noise
     frqs=[100,200,300,500]
-    T=0.5                # Generate 2-sec sample sequence
+    T=0.5                # Generate 0.5-sec sample sequence
     N=int(T*fs)
     t = np.linspace(0, T, N)
     x = np.random.normal(0, .1, N)*0.03
@@ -152,7 +177,41 @@ if True:
     fig.tight_layout()
     plt.show()
 
-
-
-
+    dy=y-yy
+    print('dy=',dy)
+    print(max(abs(dy)))
     
+##############################################################################
+
+# Example 4 - From scipy manual for lfilter
+if 0:
+
+    # Generate a noisy signal to be filtered:
+    rng = np.random.default_rng()
+    t = np.linspace(-1, 1, 201)
+    x = (np.sin(2*np.pi*0.75*t*(1-t) + 2.1) +
+         0.1*np.sin(2*np.pi*1.25*t + 1) +
+         0.18*np.cos(2*np.pi*3.85*t))
+    xn = x + rng.standard_normal(len(t)) * 0.08
+    
+    # Create an order 3 lowpass butterworth filter:
+    b, a = signal.butter(3, 0.05)
+    
+    # Apply the filter to xn. Use lfilter_zi to choose the initial condition of the filter:
+    zi = signal.lfilter_zi(b, a)
+    z, _ = signal.lfilter(b, a, xn, zi=zi*xn[0])
+    
+    # Apply the filter again, to have a result filtered at an order the same as filtfilt:
+    z2, _ = signal.lfilter(b, a, z, zi=zi*z[0])
+    
+    # Use filtfilt to apply the filter:
+    y = signal.filtfilt(b, a, xn)
+    
+    # Plot the original signal and the various filtered versions:
+    plt.figure
+    plt.plot(t, xn, 'b', alpha=0.75)
+    plt.plot(t, z, 'r--', t, z2, 'r', t, y, 'k')
+    plt.legend(('noisy signal', 'lfilter, once', 'lfilter, twice',
+                'filtfilt'), loc='best')
+    plt.grid(True)
+    plt.show()
